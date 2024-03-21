1469d4959bb76eb9a03f1f50dd13891a54a05983
JDouthitt <josiahdouthitt@gmail.com>
2024-03-20 22:46:06 -0500

    modified:   UX Hearthstone App/detail_home.js 	deleted:    UX Hearthstone App/filter.js 	modified:   UX Hearthstone App/index.html 	new file:   UX Hearthstone App/manifest.json 	modified:   UX Hearthstone App/search.html 	modified:   UX Hearthstone App/search.js 	new file:   UX Hearthstone App/service-worker.js 	modified:   UX Hearthstone App/styles.css

--- UX Hearthstone App/detail_home.js
+++ UX Hearthstone App/detail_home.js
@@ -1,3 +1,5 @@
+const featuredTemplate = document.querySelector("[featured-card-template]");
+
 const hideDetail = document.getElementById("hide-btn");
 const overlay = document.getElementById("overlay");
 const title = overlay.querySelector("[detail-name]");
@@ -8,11 +10,11 @@ const faction = overlay.querySelector("[detail-faction]");
 const race = overlay.querySelector("[detail-race]");
 const elite = overlay.querySelector("[detail-elite]");
 const collectible = overlay.querySelector("[detail-collectible]");
+const detailImage = overlay.querySelector("[detail-image]");
 
-const featured = document.getElementById("cards_featured_container");
-var featuredIDs = new Array("LT24_002H_01", "EX1_572", "EX1_572", "EX1_572");
+const featured = document.getElementById("cards-featured-container");
 
-function openDetail(cardIndex) {
+function openDetail(cardID) {
   title.textContent = "Please Wait";
   set.textContent = "";
   rare.textContent = "";
@@ -21,8 +23,8 @@ function openDetail(cardIndex) {
   race.textContent = "";
   elite.textContent = "";
   collectible.textContent = "";
+  detailImage.src = "";
 
-  const cardID = featuredIDs[cardIndex];
   const url = `https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/${cardID}`;
   const options = {
     method: "GET",
@@ -45,6 +47,10 @@ function openDetail(cardIndex) {
       race.textContent = data[0].race;
       elite.textContent = `Is Elite: ${data[0].elite}`;
       collectible.textContent = `Is Collectible: ${data[0].collectible}`;
+
+      if (data[0].img != undefined) {
+        detailImage.src = data[0].img;
+      }
     })
     .catch((error) => console.log("ERROR"));
 
@@ -55,4 +61,51 @@ function closeDetail() {
   overlay.classList.replace("overlay-slide-right", "overlay-slide-left");
 }
 
+function GetRandomCards() {
+  const characters = "abcdefghijklmnopqrstuvwxyz";
+  const first_choice = characters.charAt(
+    Math.floor(Math.random() * characters.length)
+  );
+  //const second_choice = characters.charAt(Math.floor(Math.random() * characters.length));
+
+  const url = `https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/search/${first_choice}`;
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "24b7a63a44msh1a8549990a23d2ep1dcfa8jsnb53a67af2aa7",
+      "X-RapidAPI-Host": "omgvamp-hearthstone-v1.p.rapidapi.com",
+    },
+  };
+
+  fetch(url, options)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      const useData = data.slice(0, 50);
+
+      for (let i = 0; i < 4; i++) {
+        cardData = useData[Math.floor(Math.random() * useData.length)];
+
+        const result = featuredTemplate.content.cloneNode(true).children[0];
+        const fText = result.children[1];
+        const image = result.children[0];
+
+        if (cardData.img != undefined) {
+          image.src = cardData.img;
+        }
+        fText.textContent = cardData.name;
+        result.id = cardData.cardId;
+
+        result.addEventListener("click", function () {
+          openDetail(result.id);
+        });
+
+        featured.append(result);
+      }
+    })
+    .catch((error) => console.log("ERROR"));
+}
+
 hideDetail.addEventListener("click", closeDetail);
+GetRandomCards();
--- UX Hearthstone App/search.js
+++ UX Hearthstone App/search.js
@@ -1,5 +1,16 @@
 const resultTemplate = document.querySelector("[card-search-result]");
-const resultsContainer = document.getElementById("results_container");
+const resultsContainer = document.getElementById("results-container");
+
+const searchBar = document.querySelector("[searchbar]");
+const searchButton = document.querySelector("[search-button]");
+
+const sortRarityButton = document.getElementById("sort-rarity");
+const sortClassButton = document.getElementById("sort-class");
+const sortSetButton = document.getElementById("sort-set");
+const sortFactionButton = document.getElementById("sort-faction");
+const sortTypeButton = document.getElementById("sort-type");
+const sortRaceButton = document.getElementById("sort-race");
+const noSortButton = document.getElementById("sort-none");
 
 const hideDetail = document.getElementById("hide-btn");
 const overlay = document.getElementById("overlay");
@@ -11,10 +22,15 @@ const faction = overlay.querySelector("[detail-faction]");
 const race = overlay.querySelector("[detail-race]");
 const elite = overlay.querySelector("[detail-elite]");
 const collectible = overlay.querySelector("[detail-collectible]");
+const detailImage = overlay.querySelector("[detail-image]");
 
-SearchCards("Ys");
+var currentSortButton = noSortButton;
+
+var cardData = new Array();
 
 function SearchCards(inputValue) {
+  resultsContainer.innerHTML = "";
+
   const url = `https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/search/${inputValue}`;
   const options = {
     method: "GET",
@@ -29,19 +45,8 @@ function SearchCards(inputValue) {
       return res.json();
     })
     .then((data) => {
-      data.slice(0, 50).forEach((element) => {
-        const result = resultTemplate.content.cloneNode(true).children[0];
-        const resultText = result.querySelector("[result-text]");
-
-        resultText.textContent = element.name;
-        resultText.id = element.cardId;
-
-        result.addEventListener("click", function () {
-          openDetail(element.cardId);
-        });
-
-        resultsContainer.append(result);
-      });
+      cardData = data.slice(0, 50);
+      displayData(cardData);
     })
     .catch((error) => console.log("ERROR"));
 }
@@ -55,6 +60,7 @@ function openDetail(cardID) {
   race.textContent = "";
   elite.textContent = "";
   collectible.textContent = "";
+  detailImage.src = "";
 
   const url = `https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/${cardID}`;
   const options = {
@@ -78,6 +84,10 @@ function openDetail(cardID) {
       race.textContent = data[0].race;
       elite.textContent = `Is Elite: ${data[0].elite}`;
       collectible.textContent = `Is Collectible: ${data[0].collectible}`;
+
+      if (data[0].img != undefined) {
+        detailImage.src = data[0].img;
+      }
     })
     .catch((error) => console.log("ERROR"));
 
@@ -88,4 +98,367 @@ function closeDetail() {
   overlay.classList.replace("overlay-slide-right", "overlay-slide-left");
 }
 
+function displayData(data) {
+  data.forEach((element) => {
+    const result = resultTemplate.content.cloneNode(true).children[0];
+    const resultText = result.querySelector("[result-text]");
+
+    resultText.textContent = element.name;
+
+    if (currentSortButton === sortRarityButton) {
+      resultText.textContent += " - " + element.rarity;
+    } else if (currentSortButton === sortClassButton) {
+      resultText.textContent += " - " + element.playerClass;
+    } else if (currentSortButton === sortSetButton) {
+      resultText.textContent += " - " + element.cardSet;
+    } else if (currentSortButton === sortFactionButton) {
+      resultText.textContent += " - " + element.faction;
+    } else if (currentSortButton === sortTypeButton) {
+      resultText.textContent += " - " + element.type;
+    } else if (currentSortButton === sortRaceButton) {
+      resultText.textContent += " - " + element.race;
+    }
+
+    result.addEventListener("click", function () {
+      openDetail(element.cardId);
+    });
+
+    resultsContainer.append(result);
+  });
+}
+
+function displayByRarity() {
+  if (currentSortButton === sortRarityButton) {
+    return;
+  }
+
+  currentSortButton.classList.remove("active");
+  currentSortButton = sortRarityButton;
+  sortRarityButton.classList.add("active");
+
+  const url = "https://omgvamp-hearthstone-v1.p.rapidapi.com/info";
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "24b7a63a44msh1a8549990a23d2ep1dcfa8jsnb53a67af2aa7",
+      "X-RapidAPI-Host": "omgvamp-hearthstone-v1.p.rapidapi.com",
+    },
+  };
+
+  fetch(url, options)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      rarityList = new Array();
+
+      for (let i = 0; i < data.qualities.length; i++) {
+        list = new Array();
+
+        cardData.forEach((element) => {
+          if (element.rarity === data.qualities[i]) {
+            list.push(element);
+          }
+        });
+
+        rarityList.push(list);
+      }
+
+      result = new Array();
+
+      rarityList.forEach((element) => {
+        element.forEach((card) => {
+          result.push(card);
+        });
+      });
+
+      ClearResults();
+      displayData(result);
+    })
+    .catch((error) => console.log("ERROR"));
+}
+
+function displayByClass() {
+  if (currentSortButton === sortClassButton) {
+    return;
+  }
+
+  currentSortButton.classList.remove("active");
+  currentSortButton = sortClassButton;
+  sortClassButton.classList.add("active");
+
+  const url = "https://omgvamp-hearthstone-v1.p.rapidapi.com/info";
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "24b7a63a44msh1a8549990a23d2ep1dcfa8jsnb53a67af2aa7",
+      "X-RapidAPI-Host": "omgvamp-hearthstone-v1.p.rapidapi.com",
+    },
+  };
+
+  fetch(url, options)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      classList = new Array();
+
+      for (let i = 0; i < data.classes.length; i++) {
+        list = new Array();
+
+        cardData.forEach((element) => {
+          if (element.playerClass === data.classes[i]) {
+            list.push(element);
+          }
+        });
+
+        classList.push(list);
+      }
+
+      result = new Array();
+
+      classList.forEach((element) => {
+        element.forEach((card) => {
+          result.push(card);
+        });
+      });
+
+      ClearResults();
+      displayData(result);
+    })
+    .catch((error) => console.log("ERROR"));
+}
+
+function displayBySet() {
+  if (currentSortButton === sortSetButton) {
+    return;
+  }
+
+  currentSortButton.classList.remove("active");
+  currentSortButton = sortSetButton;
+  sortSetButton.classList.add("active");
+
+  const url = "https://omgvamp-hearthstone-v1.p.rapidapi.com/info";
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "24b7a63a44msh1a8549990a23d2ep1dcfa8jsnb53a67af2aa7",
+      "X-RapidAPI-Host": "omgvamp-hearthstone-v1.p.rapidapi.com",
+    },
+  };
+
+  fetch(url, options)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      setList = new Array();
+
+      for (let i = 0; i < data.sets.length; i++) {
+        list = new Array();
+
+        cardData.forEach((element) => {
+          if (element.cardSet === data.sets[i]) {
+            list.push(element);
+          }
+        });
+
+        setList.push(list);
+      }
+
+      result = new Array();
+
+      setList.forEach((element) => {
+        element.forEach((card) => {
+          result.push(card);
+        });
+      });
+
+      ClearResults();
+      displayData(result);
+    })
+    .catch((error) => console.log("ERROR"));
+}
+
+function displayByFaction() {
+  if (currentSortButton === sortFactionButton) {
+    return;
+  }
+
+  currentSortButton.classList.remove("active");
+  currentSortButton = sortFactionButton;
+  sortFactionButton.classList.add("active");
+
+  const url = "https://omgvamp-hearthstone-v1.p.rapidapi.com/info";
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "24b7a63a44msh1a8549990a23d2ep1dcfa8jsnb53a67af2aa7",
+      "X-RapidAPI-Host": "omgvamp-hearthstone-v1.p.rapidapi.com",
+    },
+  };
+
+  fetch(url, options)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      factionList = new Array();
+
+      for (let i = 0; i < data.factions.length; i++) {
+        list = new Array();
+
+        cardData.forEach((element) => {
+          if (element.faction === data.factions[i]) {
+            list.push(element);
+          }
+        });
+
+        factionList.push(list);
+      }
+
+      result = new Array();
+
+      factionList.forEach((element) => {
+        element.forEach((card) => {
+          result.push(card);
+        });
+      });
+
+      ClearResults();
+      displayData(result);
+    })
+    .catch((error) => console.log("ERROR"));
+}
+
+function displayByType() {
+  if (currentSortButton === sortTypeButton) {
+    return;
+  }
+
+  currentSortButton.classList.remove("active");
+  currentSortButton = sortTypeButton;
+  sortTypeButton.classList.add("active");
+
+  const url = "https://omgvamp-hearthstone-v1.p.rapidapi.com/info";
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "24b7a63a44msh1a8549990a23d2ep1dcfa8jsnb53a67af2aa7",
+      "X-RapidAPI-Host": "omgvamp-hearthstone-v1.p.rapidapi.com",
+    },
+  };
+
+  fetch(url, options)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      typeList = new Array();
+
+      for (let i = 0; i < data.types.length; i++) {
+        list = new Array();
+
+        cardData.forEach((element) => {
+          if (element.type === data.types[i]) {
+            list.push(element);
+          }
+        });
+
+        typeList.push(list);
+      }
+
+      result = new Array();
+
+      typeList.forEach((element) => {
+        element.forEach((card) => {
+          result.push(card);
+        });
+      });
+
+      ClearResults();
+      displayData(result);
+    })
+    .catch((error) => console.log("ERROR"));
+}
+
+function displayByRace() {
+  if (currentSortButton === sortRaceButton) {
+    return;
+  }
+
+  currentSortButton.classList.remove("active");
+  currentSortButton = sortRaceButton;
+  sortRaceButton.classList.add("active");
+
+  const url = "https://omgvamp-hearthstone-v1.p.rapidapi.com/info";
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "24b7a63a44msh1a8549990a23d2ep1dcfa8jsnb53a67af2aa7",
+      "X-RapidAPI-Host": "omgvamp-hearthstone-v1.p.rapidapi.com",
+    },
+  };
+
+  fetch(url, options)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      raceList = new Array();
+
+      for (let i = 0; i < data.races.length; i++) {
+        list = new Array();
+
+        cardData.forEach((element) => {
+          if (element.race === data.races[i]) {
+            list.push(element);
+          }
+        });
+
+        raceList.push(list);
+      }
+
+      result = new Array();
+
+      raceList.forEach((element) => {
+        element.forEach((card) => {
+          result.push(card);
+        });
+      });
+
+      ClearResults();
+      displayData(result);
+    })
+    .catch((error) => console.log("ERROR"));
+}
+
+function ClearResults() {
+  resultsContainer.innerHTML = "";
+}
+
+searchButton.addEventListener("click", function () {
+  if (searchBar.value.trim() != "") {
+    SearchCards(searchBar.value.trim());
+  }
+});
+
 hideDetail.addEventListener("click", closeDetail);
+
+sortRarityButton.addEventListener("click", displayByRarity);
+sortClassButton.addEventListener("click", displayByClass);
+sortSetButton.addEventListener("click", displayBySet);
+sortFactionButton.addEventListener("click", displayByFaction);
+sortTypeButton.addEventListener("click", displayByType);
+sortRaceButton.addEventListener("click", displayByRace);
+
+noSortButton.addEventListener("click", function () {
+  if (currentSortButton != noSortButton) {
+    currentSortButton.classList.remove("active");
+    currentSortButton = noSortButton;
+    noSortButton.classList.add("active");
+
+    ClearResults();
+    displayData(cardData);
+  }
+});
